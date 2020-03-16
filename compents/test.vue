cd66cb01696b9a654a2f773f1cf2a812b47803f1
guoxueli <1479426829@qq.com>
2020-03-16 23:05:55 +0800

    这是一个uniapp学习项目

---
+++ compents/test.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<text>我是test组件{{msg}}</text>
+		<button type="primary" @click="test()">点击触发事件</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		props: {
+			msg: {
+				type: String,
+				default: ''
+			}
+		},
+		methods: {
+			test() {
+				this.$emit('handleClick', {
+					str: 'etwet'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="content">
+
+		<scroll-view>
+			<view class="box" v-for="(item,index) in list" :key="index">{{item.title}}</view>
+		</scroll-view>
+		<scroll-view>
+			<view v-for="(item,index) in list" :key="index">
+				<view>{{item.title}}</view>
+				<view v-for="(i,v) in item" :key="v">{{i}}</view>
+			</view>
+		</scroll-view>
+
+
+	</view>
+</template>
+
+<script>
+	import test from '../../compents/test.vue'
+	export default {
+		components: {
+			test
+		},
+		data() {
+			return {
+				title: 'Hello',
+				list: [{
+						title: '女装',
+						list: ['上衣', '短裤', '牛仔裤', '短袖', '长袖']
+					},
+					{
+						title: '童装',
+						list: ['上衣', '短裤', '牛仔裤', '短袖', '长袖']
+					},
+					{
+						title: '男装',
+						list: ['上衣', '短裤', '牛仔裤', '短袖', '长袖']
+					},
+					{
+						title: '老年装',
+						list: ['上衣', '短裤', '牛仔裤', '短袖', '长袖']
+					},
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			handleClick(rel) {
+				console.log(rel)
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+	}
+</style>
